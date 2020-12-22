ebd96f7a43ded017d1c0dbdb9275704e5516109a
Tron <tron@Trons-MacBook-Pro.local>
2020-12-22 16:43:45 -0700

    Add querying of explorer

--- check_bchain.py
+++ check_bchain.py
@@ -2,6 +2,7 @@ import json
 import re
 import requests
 import argparse
+import time
 
 var_re = re.compile('var (.+?) = (.+?);')
 
@@ -12,23 +13,27 @@ def main():
     ap.add_argument('--ignore-no-tx', action='store_true')
     args = ap.parse_args()
     for line in open(args.file):
+        line_arr = line.split(' ')
+        line = line_arr[0]
         line = line.strip()
-        r = requests.get('https://bchain.info/%s/addr/%s' % (args.coin, line))
+        print(line)
+        r = requests.get('https://www.bitgo.com/api/v1/address/%s' % (line))
         if r.status_code == 404:
             continue
         vs = {}
-        for m in var_re.finditer(r.text):
-            key, value = m.groups()
-            if key == 'startTime':
-                continue
-            try:
-                value = json.loads(value.replace('\'', '"'))
-            except json.JSONDecodeError:
-                pass
-            vs[key] = value
-        if args.ignore_no_tx and vs['total_tx'] == 0:
-            continue
-        print(vs)
+        print(r.text)
+        time.sleep(1)
+        #for m in var_re.finditer(r.text):
+        #    key, value = m.groups()
+        #    if key == 'startTime':
+        #        continue
+        #    try:
+        #        value = json.loads(value.replace('\'', '"'))
+        #    except json.JSONDecodeError:
+        #        pass
+        #    vs[key] = value
+        #if args.ignore_no_tx and vs['total_tx'] == 0:
+        #    continue
 
 if __name__ == '__main__':
     main()
